02fa46f0201de4a4bcbab7b7f9117f1521ff219d
Ma11hewThomas <25988124+Ma11hewThomas@users.noreply.github.com>
2026-06-07 15:27:25 +0100

    use jasmine tests and align ci (#55)

---
+++ spec/support/jasmine.mjs
@@ -0,0 +1,8 @@
+export default {
+	spec_dir: "test",
+	spec_files: ["**/*.test.js"],
+	helpers: ["helpers/**/*.js"],
+	env: {
+		random: false,
+	},
+};
---
+++ test/generate-report.test.js
@@ -0,0 +1,5 @@
+describe("jasmine-ctrf-json-reporter", () => {
+	it("runs with the native Jasmine test framework", () => {
+		expect(true).toBeTrue();
+	});
+});
